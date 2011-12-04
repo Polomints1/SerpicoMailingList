59da6eccb0697e6111ce47649245cc4751fed3da
Polomints1 <Polomints1@gmail.com>
2011-12-04 15:14:28 +0000

    Update inc/dbConstants.php

--- inc/dbConstants.php
+++ inc/dbConstants.php
@@ -4,8 +4,8 @@ Part of the code for the article "Use Ajax and PHP to Build Your Mailing List"
 by Aarron Walter (aarron@aarronwalter.com)
 http://www.sitepoint.com/article/use-ajax-php-build-mailing-list
 */
-define("DBHOST", "10.194.111.8");
-define("DBNAME", "db_27a78747");
-define("DBUSER", "user_27a78747");
-define("DBPASS", "P!@jlI4$d6ayq5");
+define("DBHOST", "mysql2.000webhost.com");
+define("DBNAME", "a4218432_emails");
+define("DBUSER", "a4218432_mailer");
+define("DBPASS", "aaaaaa03");
 ?>
