d0c4e2e5c4d74393004b81694579f77dc7b9d1ef
Paul <Polomints1@gmail.com>
2011-12-05 20:10:24 +0000

    adding entire project

---
+++ includes/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+// Define constants here
+
+define('DB_SERVER', '10.194.111.8');
+define('DB_USER', 'user_27a78747');
+define('DB_PASSWORD', 'P!@jlI4$d6ayq5');
+define('DB_NAME', 'db_27a78747');
--- index.php
+++ index.php
@@ -1,38 +1,38 @@
 <?php
-/*
-Part of the code for the article "Use Ajax and PHP to Build Your Mailing List"
-by Aarron Walter (aarron@aarronwalter.com)
-http://www.sitepoint.com/article/use-ajax-php-build-mailing-list
-*/
-require_once("inc/storeAddress.php");
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <title>Painless AJAX and PHP Mailing List Sign Up</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-    <script type="text/javascript" src="js/prototype.js"></script>
-    <script type="text/javascript" src="js/mailingList.js"></script>
-  </head>
-  <body>
-<?php
-if((DBHOST == "localhost") && (DBNAME == "yourdb") && (DBUSER == "youruser") && (DBPASS == "yourpass")) {
-  echo("<p>Please edit <code>inc/dbConstants.php</code> to match your MySQL configuration.</p>");
-}
-else {
+ 
+require_once 'classes/Membership.php';
+$membership = New Membership();
+ 
+$membership->confirm_Member();
+ 
 ?>
-    <form id="addressForm" action="index.php" method="get">
-      <fieldset>
-        <legend>Join our mailing list!</legend>
-        <p>
-          <input type="text" name="address" id="address" />
-          <input type="submit" value="Sign Up" />
-        </p>
-        <p id="response"><?php echo(storeAddress()); ?></p>
-      </fieldset>
-    </form>
-    <p><a href="createTable.php">Need to create a database table?</a></p>
-<?php
-}
-?>
-  </body>
+ 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+ 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/default.css" />
+ 
+<!--[if lt IE 7]>
+<script type="text/javascript" src="js/DD_belatedPNG_0.0.7a-min.js"></script>
+<![endif]-->
+ 
+ 
+<title>Untitled Document</title>
+ 
+ 
+ 
+</head>
+ 
+<body>
+ 
+<div id="container">
+<p>
+ 
+</p>
+<a href="login.php?status=loggedout">Log Out</a>
+</div><!--end container-->
+ 
+</body>
 </html>
